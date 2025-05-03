47d2894a1c17f4187341bab8c98ab13b52d7f76a
ddhruvgupta <ddhruvgupta@gmail.com>
2025-05-03 17:49:24 -0400

    updates

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,8 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme'; // Import the theme from theme.js
 import 'bootstrap/dist/css/bootstrap.min.css';
+// ErrorBoundary is a custom component that catches JavaScript errors anywhere in its child component tree,
+// logs those errors, and displays a fallback UI instead of crashing the whole app.
 import ErrorBoundary from './components/ErrorBoundary'; // Import the ErrorBoundary
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
